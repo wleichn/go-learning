2f2affe092eb5078b5c1b0771baa8e2f34a26f59
wanglei <wangleijava@dazhuanjia.com>
2020-05-29 14:59:43 +0800

    fix: 重构测试代码

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // 告诉测试套件这个方法是辅助函数（helper） 这样失败时报告的行号会是函数调用中而不是辅助函数内部
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
