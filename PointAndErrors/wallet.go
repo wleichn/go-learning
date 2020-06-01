a754c291e60e9367bda7897d5df7371b519dab80
wanglei <wangleijava@dazhuanjia.com>
2020-06-01 10:41:10 +0800

    feature: 指针

---
+++ PointAndErrors/wallet.go
@@ -0,0 +1,18 @@
+package PointAndErrors
+
+import (
+	"fmt"
+)
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Println("address of balance in test is", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
