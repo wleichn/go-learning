86fc77b4ab3669254a8123d2dd073af8bac51f9a
wleichn <wleichn@163.com>
2020-12-24 17:32:22 +0800

    基础服务端实现

--- src/zinx/znet/server.go
+++ src/zinx/znet/server.go
@@ -1,6 +1,10 @@
 package znet
 
-import "zinx/ziface"
+import (
+	"fmt"
+	"net"
+	"zinx/ziface"
+)
 
 // iServer的实现，定义一个Server的服务器模块
 type Server struct{
@@ -14,15 +18,65 @@ type Server struct{
 	Port int
 }
 func (s *Server) Start() {
+	fmt.Printf("[Start] Server Listener at IP:%s, Port %d, is Starting\n", s.IP, s.Port)
+
+	go func(){
+		// 1 获取一个TCP的套接字
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("ResolveTCPAddr err ", err)
+			return
+		}
+
+		// 2 监听服务器地址
+		listener, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen ", s.IPVersion, "err ", err)
+		}
+
+
+		// 3 阻塞的等待客户端连接，处理客户端连接业务
+		for {
+			// 如果客户端有链接过来， 阻塞返回
+			conn, err := listener.AcceptTCP()
+			if err != nil{
+				fmt.Println("Accept err ", err)
+			}
+
+			// 已经与客户端建立连接， 实现业务
+			go func(){
+				for{
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil{
+						fmt.Println("conn read err ", err)
+						return
+					}
+
+					// 回显功能
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("Write buf back err ", err)
+						return
+					}
+				}
+			}()
+		}
+	}()
 
 }
 
 func (s *Server) Stop() {
-
+	// TODO 将服务器资源、状态或者一些已经开辟的连接信息停止回收
 }
 
 func (s *Server) Serve() {
+	// 启动server的服务功能
+	s.Start()
 
+	// TODO 做一些启动服务器后的额外业务  框架扩展
+
+	//阻塞状态
+	select{}
 }
 /*
 初始化服务器的方法
